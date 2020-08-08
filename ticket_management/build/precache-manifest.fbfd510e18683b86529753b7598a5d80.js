28c26ca711c74c6c005d85c741289d91eb3e2c78
Gunjan6788 <gunjan6788@gmail.com>
2020-08-08 20:22:21 +0530

    made build file

---
+++ ticket_management/build/precache-manifest.fbfd510e18683b86529753b7598a5d80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b472e7106ed507f768fe83dc142786",
+    "url": "./index.html"
+  },
+  {
+    "revision": "80433948f543503541d4",
+    "url": "./static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "80433948f543503541d4",
+    "url": "./static/js/2.c9448a0e.chunk.js"
+  },
+  {
+    "revision": "bc99defb6e808b90e03823daf91f065b",
+    "url": "./static/js/2.c9448a0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84c87d66f89778e62ad9",
+    "url": "./static/js/main.438425b5.chunk.js"
+  },
+  {
+    "revision": "d793b57b57db432faa2e",
+    "url": "./static/js/runtime-main.2a291be3.js"
+  }
+]);
